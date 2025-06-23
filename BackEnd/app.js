cf8566f97c0a594058cd47e37f35595321692414
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-06-23 15:50:27 +0530

    Basic User Model and Controller Setup

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+const connectToDb = require("./db/db");
+connectToDb();
+
 app.use(cors());
 
 app.get("/", (req, res) => {
